c886b0999db23e17113bac09d82df485e81d4f18
jkhiani <j.khiani1@gmail.com>
2019-03-20 18:57:27 +0800

    editcommandparsertest reorganize imports

--- src/test/java/seedu/address/logic/parser/EditCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/EditCommandParserTest.java
@@ -54,10 +54,10 @@ import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
-import seedu.address.model.person.Phone;
-import seedu.address.model.person.Salary;
 import seedu.address.model.person.Occupation;
+import seedu.address.model.person.Phone;
 import seedu.address.model.person.Relationship;
+import seedu.address.model.person.Salary;
 import seedu.address.model.tag.Tag;
 import seedu.address.testutil.EditPersonDescriptorBuilder;
 
