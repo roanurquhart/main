298b7c899da21ae68b18255594aaf67350e21925
ZhangJiayu0303 <43482553+ZhangJiayu0303@users.noreply.github.com>
2019-04-02 23:10:37 +0800

    Update AddressBookParser.java
    modify

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -6,7 +6,29 @@ import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import seedu.address.logic.commands.*;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.AddCpnyCommand;
+import seedu.address.logic.commands.AverageCommand;
+import seedu.address.logic.commands.ClearCommand;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CountCommand;
+import seedu.address.logic.commands.DelFavoriteCommand;
+import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.DeleteCpnyCommand;
+import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.EditCpnyCommand;
+import seedu.address.logic.commands.ExitCommand;
+import seedu.address.logic.commands.FavoriteCommand;
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.FindCpnyCommand;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.HistoryCommand;
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.ListCpnyCommand;
+import seedu.address.logic.commands.ListFavoritesCommand;
+import seedu.address.logic.commands.RedoCommand;
+import seedu.address.logic.commands.SelectCommand;
+import seedu.address.logic.commands.UndoCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
