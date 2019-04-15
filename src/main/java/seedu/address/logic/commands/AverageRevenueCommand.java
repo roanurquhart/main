79592495d45c5a894bce359ae5f3f8988ef3e3a6
ZhangJiayu0303 <43482553+ZhangJiayu0303@users.noreply.github.com>
2019-04-15 09:33:56 +0800

    upda

--- src/main/java/seedu/address/logic/commands/AverageRevenueCommand.java
+++ src/main/java/seedu/address/logic/commands/AverageRevenueCommand.java
@@ -1,7 +1,29 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
 /**
- * Analyze
+ * Calculates the average of all avenues
  */
-public class AnalyzeCommand {
+public class AverageRevenueCommand extends Command {
+
+    public static final String COMMAND_WORD = "averageRevenue";
+
+    public static final String MESSAGE_SUCCESS = "Showed the average revenue of all persons: %.2f";
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        requireNonNull(model);
+        int sum = 0;
+        for (int i = 0; i < model.getFilteredCompanyList().size(); i++) {
+            sum += Integer.parseInt(String.valueOf(model.getFilteredCompanyList().get(i).getSalary()));
+        }
+
+        return new CommandResult(
+                String.format(MESSAGE_SUCCESS, (double) sum / model.getFilteredCompanyList().size()));
+
+
+    }
 }
