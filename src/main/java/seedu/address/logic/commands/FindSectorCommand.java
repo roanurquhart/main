c94888541d06ab93ddb96efd87a61e2ca116bb73
ZhangJiayu0303 <43482553+ZhangJiayu0303@users.noreply.github.com>
2019-04-15 10:16:18 +0800

    Update FindSectorCommand.java

--- src/main/java/seedu/address/logic/commands/FindSectorCommand.java
+++ src/main/java/seedu/address/logic/commands/FindSectorCommand.java
@@ -1,4 +1,41 @@
 package seedu.address.logic.commands;
 
-public class FindSectorCommand {
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+import seedu.address.model.person.SectorContainsKeywordsPredicate;
+
+/**
+ * Finds and lists all companies in address book whose sector contains any of the argument keywords.
+ * Keyword matching is case insensitive.
+ */
+public class FindSectorCommand extends Command {
+
+    public static final String COMMAND_WORD = "findSector";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all companies with exact sector. (Case insensitive) "
+            + "Example: " + COMMAND_WORD + " bank";
+
+    private final SectorContainsKeywordsPredicate predicate;
+
+    public FindSectorCommand(SectorContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        requireNonNull(model);
+        model.updateFilteredCompanyList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredCompanyList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof FindSectorCommand // instanceof handles nulls
+                && predicate.equals(((FindSectorCommand) other).predicate)); // state check
+    }
 }
